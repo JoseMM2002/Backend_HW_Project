0cb2305002dbf7ab07b665d8ddc61cdbddce4e0e
JoseMM2002 <jeolmmt@gmail.com>
2024-04-19 14:18:11 -0600

    sync accounts, cards and transactions in syncdb function

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,18 +1,24 @@
-import { Sequelize } from "sequelize"
-import { syncUsers } from "./models/User";
+import { Sequelize } from 'sequelize';
+import { syncUsers } from './models/User';
+import { syncAccounts } from './models/Account';
+import { syncCards } from './models/Card';
+import { syncTransactions } from './models/Transaction';
 
 export const sequilize = async () => {
     const sequilize = new Sequelize({
-        dialect: "sqlite",
+        dialect: 'sqlite',
         storage: './db.sqlite',
         logging: console.log
-    })
+    });
     await sequilize.authenticate();
     return sequilize;
-}
+};
 
 export const syncDb = async () => {
     const db = await sequilize();
     syncUsers(db);
+    syncAccounts(db);
+    syncCards(db);
+    syncTransactions(db);
     await db.sync();
-}
+};
